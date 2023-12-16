06e303fbffac2ae3a2336b20dc9643a027187c4b
Marcello Crescimbeni <giosuemc@gmail.com>
2023-12-16 01:30:50 -0500

    prueba

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida, ajusta según sea necesario
+  },
+  base: '/nombre-de-tu-repositorio/', // Ajusta esto según tu repositorio en GitHub
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+  },
+  resolve: {
+    alias: {}
+  }
 })
